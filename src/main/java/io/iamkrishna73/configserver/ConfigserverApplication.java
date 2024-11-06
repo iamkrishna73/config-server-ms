6a9f4fbfd6554967ef90a20b3f5aff0f6a7fc06e
krishna <krishnasingj137333@gmail.com>
2024-11-07 00:12:19 +0530

    first commit

---
+++ src/main/java/io/iamkrishna73/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package io.iamkrishna73.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+}
---
+++ src/test/java/io/iamkrishna73/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iamkrishna73.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
